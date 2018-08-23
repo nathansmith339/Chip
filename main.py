1c9c56b968ec94bbd9757029c6b1ee60f9c00fce
Nathan Smith <smithnathan339@gmail.com>
2018-08-22 22:24:09 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    # user types '!foo', bot says 'bar'
+    if message.content.startswith('!foo'):
+        await client.send_message(destination=message.channel, content='bar')
+
+client.run('NDgyMDM0ODE0NDE3ODk1NDM0.Dl_O3w.Zm8LBKK-mxmxdUNsDe52AK1yavw')
